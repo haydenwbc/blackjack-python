da94d09ad1341bb132b2b3e633976a864eb7a81c
William Hayden <haydenw@bc.edu>
2023-10-16 16:25:31 -0400

    Initial Commit of game.py

---
+++ src/utils.py
@@ -0,0 +1,3 @@
+#Definition of SUITS and VALUES constants for deck generation: 
+SUITS = ["Hearts", "Clubs", "Diamonds", "Spades"]
+VALUES = ['2', '3', '4', '5', '6', '7', '8', '9', '10', "Jack", "Queen", "King", "Ace"]
