7ea98cd5df65a2b55b955bd24980ebb4a6a22739
William Hayden <haydenw@bc.edu>
2023-10-16 20:38:50 -0400

    pygame test plus image added to assets

---
+++ src/pygame_test.py
@@ -0,0 +1,18 @@
+## run pip install pygame
+import pygame
+
+pygame.init()
+screen = pygame.display.set_mode((900,450)) #Resolution: width, height
+pygame.display.set_caption('Blackjack') #Title
+clock = pygame.time.Clock()
+
+table_surface = pygame.image.load('assets/table.png')
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            exit()
+    screen.blit(table_surface, (0,0)) #block image transfer (one image on another image)      0,0 is at top left corner of box screen      
+    pygame.display.update()
+    clock.tick(60) #Framerate
